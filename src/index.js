2d30ecc22a0b36bfa544387d37f51d0976e20bd8
proconectach38 <contact@proconecta.com>
2023-11-27 17:14:46 +0200

    FE - added AppRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+// Routers
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // redux
 import { Provider } from 'react-redux';
@@ -11,16 +14,19 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
 // components
-import App from './components/App';
+import AppRouter from './components/AppRouter';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <AppRouter />
+        </Router>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
